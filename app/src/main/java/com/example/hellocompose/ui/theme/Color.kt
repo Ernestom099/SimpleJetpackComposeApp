ca1ce4d31506fb45f64f6fee8cc88ed160d561c2
Ernestom099 <82059490+Ernestom099@users.noreply.github.com>
2022-04-29 11:28:41 -0500

    Update: App Created

---
+++ app/src/main/java/com/example/hellocompose/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.hellocompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFB967B1)
+val Purple500 = Color(0xFF205E29)
+val Purple700 = Color(0xFFB0DA64)
+val Teal200 = Color(0xFF02FDE5)
+val Title = Color(0xFF000000)
+val FontText = Color(0xFF858484)
